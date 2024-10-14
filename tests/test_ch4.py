757eadaf3a3119ffdba418f60b4d2243179a6458
Ali Gheshlaghi <aliigheshlaghi97@gmail.com>
2024-10-14 21:45:30 +0330

    fix(ex_4_1): modified tests to pass logically.

--- tests/test_ch4.py
+++ tests/test_ch4.py
@@ -6,24 +6,27 @@ import unittest
 from unittest.mock import patch, AsyncMock, MagicMock
 
 sys.path.append(os.path.abspath('docs/4. Synchronization and Coordination'))
-from ex_4_1 import task as task_ex_1, main as main_ex_1
+import ex_4_1 as ex1
 
 
 class TestEx1AsyncTask(unittest.TestCase):
     @patch('asyncio.sleep', return_value=None)
     def test_task(self, mock_sleep):
         """Test task function."""
-        global value
-        value = 0
-        asyncio.run(task_ex_1())
+        ex1.value = 0
 
-        self.assertEqual(value, 0)  # TODO: this must equal with 1 not 0
+        asyncio.run(ex1.task())
+        assert ex1.value == 1
+
+        asyncio.run(ex1.task())
+        assert ex1.value == 2
+
         mock_sleep.assert_called_with(0.01)
 
     def test_main_success(self):
         """Test main function"""
         with patch('builtins.print') as mocked_print:
-            asyncio.run(main_ex_1())
+            asyncio.run(ex1.main())
             mocked_print.assert_any_call(1)
 
 
